5ec3fa536546f1fb18e18292c9c710c6ab10eb30
evildiabriek <ritagabrieldaniel@gmail.com>
2026-08-27 17:21:16 +0200

    Adding Error

--- src/error.zig
+++ src/error.zig
@@ -4,16 +4,18 @@ pub const EError = enum {
     LogsDontContains,
     LenReachedDestLen,
     HomeVariableEnvironnementDontExist,
+    RobloxLogDirNotValide,
 };
 
 const _errortab = std.EnumArray(EError, []const u8).init(.{
     .LogsDontContains = "Logs dont contains the researched word",
     .LenReachedDestLen = "The len is superior or equal then dest.len",
     .HomeVariableEnvironnementDontExist = "The variable HOME or USERPROFILE doesnt exit",
+    .RobloxLogDirNotValide = "Logs dont contain obligatoire charactere (/ or \\)",
 });
 
 pub fn print_error(eerr: EError, note: []const u8, exit: bool) void {
-    std.debug.print("ERROR: {s}\n", .{_errortab.get(eerr)});
-    std.debug.print("Developper note: {s}\n", .{note});
+    std.log.err("ERROR: {s}", .{_errortab.get(eerr)});
+    std.log.err("Developper note: {s}", .{note});
     if (exit) std.process.exit(1);
 }
